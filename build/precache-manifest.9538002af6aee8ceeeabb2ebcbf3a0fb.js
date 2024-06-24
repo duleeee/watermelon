cf67573e4fa8d786b47863a3264ff839ed42237e
duleeee <duleee@gmail.com>
2024-06-24 13:39:03 +0200

    Add files via upload

---
+++ build/precache-manifest.9538002af6aee8ceeeabb2ebcbf3a0fb.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b2d0251d42357959449d5d5396e1a01f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0bc1999c3ceb7d7ed722",
+    "url": "/static/css/main.bc0029cf.chunk.css"
+  },
+  {
+    "revision": "52dac75e7272f7def445",
+    "url": "/static/js/2.7c9ee01c.chunk.js"
+  },
+  {
+    "revision": "7ab60fc7ce12f3f41afed51aedbbb95a",
+    "url": "/static/js/2.7c9ee01c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aa8a24aad81fe09fc845",
+    "url": "/static/js/3.ef61d17f.chunk.js"
+  },
+  {
+    "revision": "0bc1999c3ceb7d7ed722",
+    "url": "/static/js/main.1db1a4fd.chunk.js"
+  },
+  {
+    "revision": "1055cdeecedca4fce09b",
+    "url": "/static/js/runtime-main.969696ed.js"
+  },
+  {
+    "revision": "71cc0b9712f7f4757f98d6513fab06b7",
+    "url": "/static/media/party.71cc0b97.jpg"
+  },
+  {
+    "revision": "8607c0e1c1851e481d29230ed3b7c574",
+    "url": "/static/media/watermelon-seeds.8607c0e1.png"
+  }
+]);
